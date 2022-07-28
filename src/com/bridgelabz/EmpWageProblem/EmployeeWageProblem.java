7baf78d7b103b733d23d89ade703158f8f95c37d
iamtalib13 <talibsheikh16@gmail.com>
2022-07-29 00:33:56 +0530

    Added - Welcome Message on Master Branch

---
+++ src/com/bridgelabz/EmpWageProblem/EmployeeWageProblem.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.EmpWageProblem;
+
+public class EmployeeWageProblem {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee Wage Computation Program");
+
+
+    }
+}
